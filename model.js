66a18cb641c9ff7e2727c01aa122cd97f1576aa9
Kyle Dolezal & McEileen <pair+KyleDolezal.McEileen@devbootcamp.com>
2015-11-16 13:56:49 -0600

    appear to recognize italics formatting

--- model.js
+++ model.js
@@ -1,16 +1,4 @@
 var parseLines = function(line) {
-  // if (line.charAt(0) == "#"){
-  //   console.log('----------------')
-  //   console.log('yes')
-  //   console.log(line);
-  //   console.log('----------------')
-  // }
-  // else {
-  //   console.log('----------------')
-  //   console.log('no')
-  //   console.log(line);
-  //   console.log('----------------')
-  // }
   if (line.substring(0, 3) == "###"){
     return ("<h3>" + line.slice(3) + "</h3><br>");
   }
@@ -20,14 +8,14 @@ var parseLines = function(line) {
   else if (line.charAt(0) == "#"){
     return ("<h1>" + line.slice(1) + "</h1><br>");
   }
-  else {
-    return (line + "<br>");
-  }
-  if (line.indexOf('*') != -1) {
+  else if (line.indexOf('*') != -1) {
       var firstStar = line.indexOf('*');
       console.log(firstStar);
-    if (line.includes("*", firstStar)) {
-      alert("italics");
-    }
+      if (line.includes("*", firstStar + 1 )) {
+        alert("italics");
+      }
   }
+  else {
+    return (line + "<br>");
+  };
 };
