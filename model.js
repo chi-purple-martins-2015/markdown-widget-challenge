89c4a3618150b669daedc111989b5db7897f72ba
Kyle Dolezal & McEileen <pair+KyleDolezal.McEileen@devbootcamp.com>
2015-11-16 13:53:53 -0600

    start work on displaying italics

--- model.js
+++ model.js
@@ -12,21 +12,22 @@ var parseLines = function(line) {
   //   console.log('----------------')
   // }
   if (line.substring(0, 3) == "###"){
-    console.log("heading3")
-    console.log(line.slice(3));
     return ("<h3>" + line.slice(3) + "</h3><br>");
   }
   else if (line.substring(0, 2) == "##"){
-    console.log("heading2")
-    console.log(line.slice(2));
     return ("<h2>" + line.slice(2) + "</h2><br>");
   }
   else if (line.charAt(0) == "#"){
-    console.log("heading1")
-    console.log(line.slice(1));
     return ("<h1>" + line.slice(1) + "</h1><br>");
   }
   else {
-    return line;
+    return (line + "<br>");
+  }
+  if (line.indexOf('*') != -1) {
+      var firstStar = line.indexOf('*');
+      console.log(firstStar);
+    if (line.includes("*", firstStar)) {
+      alert("italics");
+    }
   }
 };
