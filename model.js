5337b2707a00b3445532fd6f4a2fc71fbd847c5b
Kyle Dolezal & McEileen <pair+KyleDolezal.McEileen@devbootcamp.com>
2015-11-16 13:33:35 -0600

    can remove diff heading sizes but still display #s

--- model.js
+++ model.js
@@ -11,10 +11,19 @@ var parseLines = function(line) {
   //   console.log(line);
   //   console.log('----------------')
   // }
-  if (line.charAt(0) != "#"){
-    return line;
+  if (line.substring(0, 3) == "###"){
+    console.log("heading3")
+    console.log(line.substring(0, 2));
+    return ("<h3>" + line + "</h3><br>");
   }
-  if (line.charAt(0) == "#"){
+  else if (line.substring(0, 2) == "##"){
+    console.log("heading2")
+    console.log(line.substring(0,1));
+    return ("<h2>" + line + "</h2><br>");
+  }
+  else if (line.charAt(0) == "#"){
+    console.log("heading1")
+    console.log(line.charAt(0));
     return ("<h1>" + line + "</h1><br>");
   }
   else {
