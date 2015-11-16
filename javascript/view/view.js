9285f6712232bb0f5dc104cf25381de373e0a7aa
Cora Hays-Magan & Lucy Boone <pair+corainchicago.lucyboone@devbootcamp.com>
2015-11-16 13:51:31 -0600

    fix converter function to work

--- javascript/view/view.js
+++ javascript/view/view.js
@@ -2,9 +2,33 @@ $(document).ready(function(){
   $("form").on("click", "button", function(e){
     e.preventDefault();
     var text = $("textarea").val();
-    $("#preview-div textarea").append(text);
+    var result = converter(text, /\*/, "i");
+    $("#preview-div p").append(result);
+    console.log("this is the jquery function")
+    console.log(text);
+    console.log(result);
   })
   $("#preview-div button").on("click", function(){
-    $("#preview-div textarea").empty();
+    $("#preview-div p").empty();
   })
 })
+
+//
+// testString = "I * heart * New York."
+
+
+var converter = function(text, symbol, styleAdd){
+  start = text.search(symbol);
+
+  beginText = text.slice((start+1), -1);
+
+  end = beginText.search(symbol);
+
+  endText = beginText.slice(0, end);
+
+  returnText = "<" + styleAdd + ">" + endText+ "</" + styleAdd + ">"
+  result = text.replace(endText, returnText);
+  result = result.replace(/\*/, "");
+  result = result.replace(/\*/, "");
+  return result;
+}
