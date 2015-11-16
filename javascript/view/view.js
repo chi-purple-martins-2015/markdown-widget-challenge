4f459ba36a781305a7c4d8ad1a1f39df5292f098
Cora Hays-Magan & Lucy Boone <pair+corainchicago.lucyboone@devbootcamp.com>
2015-11-16 14:49:29 -0600

    working converter function

--- javascript/view/view.js
+++ javascript/view/view.js
@@ -1,34 +1,33 @@
 $(document).ready(function(){
   $("form").on("click", "button", function(e){
     e.preventDefault();
-    var text = $("textarea").val();
-    var result = converter(text, /\*/, "i");
+    var result = $("textarea").val();
+    result = converter(result, /\*/, "i");
+    result = converter(result, /\_/, "i");
+    // result = converter(result, /(\*){2}/, "b");
+    result = converter(result, /\*\*/, "b");
     $("#preview-div p").append(result);
-    console.log("this is the jquery function")
-    console.log(text);
-    console.log(result);
   })
   $("#preview-div button").on("click", function(){
     $("#preview-div p").empty();
   })
 })
 
-//
-// testString = "I * heart * New York."
-
-
 var converter = function(text, symbol, styleAdd){
-  start = text.search(symbol);
-
-  beginText = text.slice((start+1), -1);
-
-  end = beginText.search(symbol);
-
-  endText = beginText.slice(0, end);
-
-  returnText = "<" + styleAdd + ">" + endText+ "</" + styleAdd + ">"
-  result = text.replace(endText, returnText);
-  result = result.replace(/\*/, "");
-  result = result.replace(/\*/, "");
-  return result;
+    var start = text.search(symbol);
+    console.log(typeof(symbol));
+    if(text[start+1]==="*"){
+      var beginText = text.slice((start+2), -1);
+    } else{
+      var beginText = text.slice((start+1), -1);
+    }
+    var end = beginText.search(symbol);
+    var endText = beginText.slice(0, end);
+    var returnText = "<" + styleAdd + ">" + endText + "</" + styleAdd + ">"
+    text = text.replace(endText, returnText);
+    text = text.replace(symbol, "");
+    text = text.replace(symbol, "");
+    return text;
 }
+
+// This is the *cat*, you _must_ love **him**.
