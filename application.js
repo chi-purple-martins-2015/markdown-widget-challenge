8527a013032bf8173660c6c86302b2bbbbc29d37
Katelyn Dinkgrave <katelyn.dinkgrave@gmail.com>
2015-11-16 11:06:34 -0600

    works with italic * and _

--- application.js
+++ application.js
@@ -1,9 +1,15 @@
 $(document).ready(function() {
   $(document).on("keyup", function(event) {
     var userMarkdown = $("textarea").val();
-    
-    userMarkdown = userMarkdown.replace(/\*\*\b/gi, " <strong>");
+
+    userMarkdown = userMarkdown.replace(/\*\*\b/gi, "<strong>");
     userMarkdown = userMarkdown.replace(/\b\*\*/gi, "</strong>");
+
+    userMarkdown = userMarkdown.replace(/\*{1}\b/gi, "<em>");
+    userMarkdown = userMarkdown.replace(/\b\*{1}/gi, "</em>");
+
+    userMarkdown = userMarkdown.replace("_", "<em>");
+    userMarkdown = userMarkdown.replace("_", "</em>");
 
     $(".generated-text").html(userMarkdown);
   });
